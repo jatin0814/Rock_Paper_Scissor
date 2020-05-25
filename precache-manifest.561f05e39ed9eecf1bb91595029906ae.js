8e3b6f75d555bb195cf55b9b1c9faf789bd0bd4a
jatin0814 <201951073@iiitvadodara.ac.in>
2020-05-25 11:16:34 +0530

    Updates

---
+++ precache-manifest.561f05e39ed9eecf1bb91595029906ae.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5435f6abb6635006e22b15e6dc521c8b",
+    "url": "/Rock_Paper_Scissor/index.html"
+  },
+  {
+    "revision": "1bdfae70d0cfb09f7756",
+    "url": "/Rock_Paper_Scissor/static/css/main.9bf8b343.chunk.css"
+  },
+  {
+    "revision": "29b961568b992b4d71a7",
+    "url": "/Rock_Paper_Scissor/static/js/2.7597fbd2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Rock_Paper_Scissor/static/js/2.7597fbd2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bdfae70d0cfb09f7756",
+    "url": "/Rock_Paper_Scissor/static/js/main.a116db64.chunk.js"
+  },
+  {
+    "revision": "a08cebe866169ab8d8a2",
+    "url": "/Rock_Paper_Scissor/static/js/runtime-main.a0ca396c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Rock_Paper_Scissor/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "acd020103d48b28a9ee769f61c042d73",
+    "url": "/Rock_Paper_Scissor/static/media/rock.acd02010.png"
+  }
+]);
